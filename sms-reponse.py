80d27f73e75990cfdec8787097414312ea4b67b9
server web <46269785+khousseinmaalov@users.noreply.github.com>
2019-10-24 19:45:43 +0000

    Update sms-reponse.py

--- sms-reponse.py
+++ sms-reponse.py
@@ -17,18 +17,24 @@ messages = service.fetch_message_history()
 a = messages[0].content
 
 while 1:
-  if message[0].phone == y:
-    if message.content != liste[l-1]:
-      service.send_message(y, liste[l])
-      a = messages[0].content
-      print(a,l)
-      l = l+1
-      if l == len(liste):
-        l = 0
-    else:
-      service.send_message(y, liste[k])
-      a = messages[0].content
-      print(a,k)
-      k = k+1
-      if k == len(listee):
-        k = 0
+  sleep(1)
+  messages = service.fetch_message_history()
+  print(a,x)
+  if a == messages[0].content:
+    pass
+  else:
+    if message[0].phone == y:
+      if message.content != liste[l-1]:
+        service.send_message(y, liste[l])
+        a = messages[0].content
+        print(a,l)
+        l = l+1
+        if l == len(liste):
+          l = 0
+      else:
+        service.send_message(y, liste[k])
+        a = messages[0].content
+        print(a,k)
+        k = k+1
+        if k == len(listee):
+          k = 0
