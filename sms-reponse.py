3726c99632368de90aa1b183b05e6cd29b8b2184
server web <46269785+khousseinmaalov@users.noreply.github.com>
2019-10-24 19:20:38 +0000

    Create sms-reponse.py

---
+++ sms-reponse.py
@@ -0,0 +1,38 @@
+from ipaddress import IPv4Address
+from pyairmore.request import AirmoreSession
+from pyairmore.services.messaging import MessagingService
+from time import sleep
+
+k = 0
+l = 0
+liste = ["oui", "non"]
+listee = ["vrai", "faux"]
+
+x = input("ip locale: ")
+y = input("numéro cible: ")
+z = input("message: ")
+snk = int(input("nb de réptésions: "))
+
+ip = IPv4Address(x)
+session = AirmoreSession(ip)
+was_accepted = session.request_authorization()
+
+service = MessagingService(session)
+a = messages[0].content
+
+while 1:
+  if message[0].phone == y:
+    if message.content != liste[l-1]:
+      service.send_message(y, liste[l])
+      a = messages[0].content
+      print(a,l)
+      l = l+1
+      if l == len(liste):
+        l = 0
+    else:
+      service.send_message(y, liste[k])
+      a = messages[0].content
+      print(a,k)
+      k = k+1
+      if k == len(listee):
+        k = 0
